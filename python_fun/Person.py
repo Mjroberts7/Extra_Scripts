834b063ee41ca5a87760f896f69f762a28631bab
MJ Roberts <michael.j.roberts33@gmail.com>
2026-01-11 23:52:17 -0600

    adding a few files for AD and Python Classes

---
+++ python_fun/Person.py
@@ -0,0 +1,19 @@
+class Person():
+    def __init__(self, name, body_type):
+        self.name = name
+        self.body_type = body_type
+
+    def head(self, nose, ears, eyes):
+        self.nose = nose
+        self.ears = ears
+        self.eyes = eyes
+
+    def hands(self, fingers):
+        self.fingers = fingers
+        number_of_fingers = 10
+        fingers_per_hand = 5
+
+    def feet(self, toes):
+        self.toes = toes
+        number_of_toes = 10
+        toes_per_foot = 5
